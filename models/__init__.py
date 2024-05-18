75eddb0fa55b8566392e189bb032a06b7a78b4b2
GracyDinma <okohagchidinma@gmail.com>
2024-05-18 10:11:58 -0700

    init files

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+from models.engine.file_storage import FileStorage
 
+"""
+create a unique filestorage instance
+"""
+storage = FileStorage()
+"""
+call relaod() method on this variable to load date from JSON file
+"""
+storage.reload()
