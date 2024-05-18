22907b1de24e6c9bab6695ba4a96bef91d5b6503
georgesmbock <mbock@DESKTOP-3KEOBHU>
2024-05-18 21:31:33 +0100

    add user class

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModeli
+from models.base_model import BaseModel
 
-storage =FileStorage()
+storage = FileStorage()
 
 storage.reload()
